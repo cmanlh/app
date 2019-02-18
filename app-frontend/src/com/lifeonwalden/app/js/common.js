f95604f65379738bd5d65fefcb94e7fe291b4318
mawenjie <mawenjie@orientsec.com.cn>
2019-02-18 09:06:44 +0800

    add $.openPage func

--- app-frontend/src/com/lifeonwalden/app/js/common.js
+++ app-frontend/src/com/lifeonwalden/app/js/common.js
@@ -237,12 +237,23 @@ $JqcLoader.importComponents('com.jquery', ['jquery', 'keycode', 'version'])
 
 
         var styleCache = {};
-        $.addForm = function(menu, tab) {
-            var uid = menu.id;
+        var jqcTab = null;
+        $.addForm = function(menu, tab, needReload) {
+            var uid = menu.url;
             var text = menu.text;
+            if (!jqcTab) {
+                jqcTab = tab;
+            }
             // tab内存在
             if (tab.index.has(uid)) {
                 tab.add({ id: uid });
+                if (needReload) {
+                    var panel = tab.index.get(uid).panel;
+                    var root = panel.find('div[data-tabid]');
+                    root.empty();
+                    var _app = $.getFormCache(uid);
+                    _app.mount(root, menu.data);
+                }
                 return;
             }
             var _path = APP_ROOT_PATH.concat(menu.url);
@@ -270,11 +281,18 @@ $JqcLoader.importComponents('com.jquery', ['jquery', 'keycode', 'version'])
                 });
                 setTimeout(function() {
                     var panel = tab.index.get(uid).panel;
-                    var root = panel.find('div[data-tabid=' + uid + ']');
-                    $.getFormCache(uid).mount(root, panel);
+                    var root = panel.find('div[data-tabid]');
+                    $.getFormCache(uid).mount(root, menu.data);
                 }, 0);
             }
         };
+        $.openPage = function (formUrl, title, data) {
+            $.addForm({
+                text: title,
+                url: formUrl,
+                data: data
+            }, jqcTab, true);
+        }
         // 缓存新加载的js文件数据
         const formCache = new Map();
         // set cache
@@ -357,7 +375,7 @@ $JqcLoader.importComponents('com.jquery', ['jquery', 'keycode', 'version'])
             $.setupApp(this);
             $.jqcEvent.emit('setup.app', this);
         };
-        $.App.prototype.mount = function (root) {
+        $.App.prototype.mount = function (root, data) {
             var _this = this;
             this._root = root;
             this.root = root;
@@ -381,13 +399,13 @@ $JqcLoader.importComponents('com.jquery', ['jquery', 'keycode', 'version'])
                         _this.__renderContextMenu();
                     }
                     if (_this.templatePath) {
-                        _this.__getTemplateAndRender();
+                        _this.__getTemplateAndRender(data);
                     } else {
                         if (_this.dxDataGrid) {
                             _this.__renderDxDataGrid();
                         }
                         // 生命周期-渲染之后
-                        _this.__afterRender();
+                        _this.__afterRender(data);
                     }
                 });
             });
@@ -453,7 +471,7 @@ $JqcLoader.importComponents('com.jquery', ['jquery', 'keycode', 'version'])
             });
             return _pathArr.join('/');
         };
-        $.App.prototype.__getTemplateAndRender = function () {
+        $.App.prototype.__getTemplateAndRender = function (data) {
             var _this = this;
             this.getFile(_this.templatePath).then(res => {
                 var _fakeBox = $('<div>').append($(res));
@@ -472,7 +490,7 @@ $JqcLoader.importComponents('com.jquery', ['jquery', 'keycode', 'version'])
                 if (_this.dxDataGrid) {
                     _this.__renderDxDataGrid();
                 }
-                _this.__afterRender();
+                _this.__afterRender(data);
             });
         };
         $.App.prototype.__renderToolBar = function () {
@@ -646,9 +664,19 @@ $JqcLoader.importComponents('com.jquery', ['jquery', 'keycode', 'version'])
                 });
             }, 0);
         };
-        $.App.prototype.__afterRender = function () {
+        $.App.prototype.__afterRender = function (data) {
             var _this = this;
             var _afterRender = [].concat(this._afterRender);
+            // 带参数渲染
+            _afterRender.splice(-1, 0, function (next) {
+                if (data) {
+                    var search = this.root.find('.toolbar-left');
+                    if (search.length) {
+                        $.formUtil.fill(search, data)
+                    }
+                }
+                next();
+            });
             setTimeout(function () {
                 queue(_afterRender, _this);
                 _this.loading.hide();
